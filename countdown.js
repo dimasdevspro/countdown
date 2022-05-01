29b12ff5759160b13209af99f8dd7b3bcdee6a54
dimasdevspro <dimasdevspro@gmail.com>
2022-05-01 17:58:17 -0300

    the first commit

---
+++ countdown.js
@@ -0,0 +1,33 @@
+let daysEl = document.getElementById("days");
+let hoursEl = document.getElementById("hours");
+let minsEl = document.getElementById("min");
+let secondsEl = document.getElementById("sec")
+
+function formatTime(time){
+    return time < 10 ? `0${time}` : time;
+}
+
+const newYear = "1 Jan 2023"
+
+function countdown(){
+
+    const newYearDate = new Date(newYear);
+    const currentDate = new Date();
+
+    const totalSeconds = (newYearDate - currentDate) / 1000;
+
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+    const mins = Math.floor(totalSeconds / 60) % 60;
+    const seconds = Math.floor(totalSeconds) % 60;
+
+    daysEl.innerHTML = days;
+    hoursEl.innerHTML = formatTime(hours);
+    minsEl.innerHTML = formatTime(mins);
+    secondsEl.innerHTML = formatTime(seconds);
+
+}
+
+countdown();
+
+setInterval(countdown, 1000);
